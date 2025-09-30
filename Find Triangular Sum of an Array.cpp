b92af4652ff15c459b5232301fa0a730ca69dadc
Mayur_06 <156815301+Mayurprajapati6@users.noreply.github.com>
2025-09-30 11:23:08 +0530

    Create Find Triangular Sum of an Array.cpp

---
+++ Find Triangular Sum of an Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        
+        while(nums.size() > 1) {
+            vector<int> newNum(nums.size()-1);
+            for(int i=0;i<nums.size()-1;i++){
+                newNum[i] = (nums[i] + nums[i+1]) % 10;
+            }
+            nums = newNum;
+        }
+
+        return nums[0];
+        
+    }
+};
